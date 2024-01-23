7930b321a25c747a5c18cb48392594efc47f20bf
Sheikh Umair <umair.najeeb@iquasar.com>
2024-01-23 20:18:56 +0530

    Added User Controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,44 +1,81 @@
-import userModel from "../models/userModel.js"
+import userModel from "../models/userModel.js";
 
-const getAllUsers = async (req, res)=>{
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await userModel.find();
+    res.status(200).json({
+      users,
+    });
+  } catch (error) {
+    res.status(404).json({
+      error: error.message,
+    });
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await userModel.findById(id);
+    res.status(200).json({
+      message: "User Found",
+      user,
+    });
+  } catch (error) {
+    res.status(404).json({
+      error: error.message,
+    });
+  }
+};
+
+const addUser = async (req, res) => {
+  try {
+    const user = await userModel.create(req.body);
+    res.status(201).json({
+      message: "User Created",
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+
+const updateUser = async (req, res) => {
+  try {
+
+    const id = req.params.id;
+
+    const newUser = await userModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+        message : "User Updated",
+        newUser
+    })
+  } catch (error) {
+    res.status(500).json({
+        error : error.message
+    })
+  }
+};
+
+const deleteUser = async (req, res) => {
+
     try {
-        const users = await userModel.find()
+        const id = req.params.id
+        await userModel.findByIdAndDelete(id)  
         res.status(200).json({
-            users
+            message : "User Deleted"
         })
     } catch (error) {
-        res.status(404).json({
-            error : error.message
-        }
-        )
-    }
-   
-
-}
-
-const getUser = ()=>{
-
-}
-
-const addUser = async(req, res)=>{
-    try {
-        const user = await userModel.create(req.body)
-        res.status(201).json(
-            user
-        )
-    } catch (error) {
-        res.status(500).json({
+        res.status(400).json({
             error : error.message
         })
     }
-}
+};
 
-const updateUser = () => {
-    
-}
-
-const deleteUser = () => {
-
-}
-
-export  {getAllUsers, getUser, updateUser, addUser, deleteUser}
+export { getAllUsers, getUser, updateUser, addUser, deleteUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,8 @@
 import express from "express"
-import {addUser, getAllUsers} from "../controllers/userController.js"
+import {addUser, deleteUser, getAllUsers, getUser, updateUser} from "../controllers/userController.js"
 const userRouter = express.Router()
 
 userRouter.route("/").get(getAllUsers).post(addUser)
+userRouter.route("/:id").get(getUser).put(updateUser).delete(deleteUser)
 
 export default userRouter

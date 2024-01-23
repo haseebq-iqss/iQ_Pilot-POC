ccbfeda26c026875f355c3d756f796c10da59e4e
Arshiya-iqss <arshiya.shakeel@iquasar.com>
2024-01-23 19:56:14 +0530

    fixed user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -7,7 +7,7 @@ const userSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     phone: { type: Number, required: true },
-    password: { type: Number, required: true },
+    password: { type: String, required: true },
     address: { type: String, required: true },
 
     // Additional details

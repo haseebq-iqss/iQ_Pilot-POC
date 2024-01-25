fea53041bbc78dd10804ffc8e08edcce46291943
Haseeb Qureshi - IQSS <143264149+haseebq-iqss@users.noreply.github.com>
2024-01-25 15:56:01 +0530

    URL change  + vercel Integration

--- frontend/src/hooks/useAxios.ts
+++ frontend/src/hooks/useAxios.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export const useAxios = axios.create({
-    baseURL: "http://localhost:5000/api/v1/"
+    // baseURL: "http://localhost:5000/api/v1/"
+    baseURL: "https://iq-back-cevq.onrender.com/api/v1/"
 })
